f3813ef13ce428faff3b88d00bda3163f171e665
Shubham <shubham.kumar@ibyteinfomatics.com>
2021-02-19 14:11:09 +0530

    role map seeeder update

--- Modules/User/Database/Seeders/SeedUserFieldMapRolesTableSeeder.php
+++ Modules/User/Database/Seeders/SeedUserFieldMapRolesTableSeeder.php
@@ -139,8 +139,8 @@ class SeedUserFieldMapRolesTableSeeder extends Seeder
                         array("user_field_id"=>29,'role_id'=>8,'step'=>'step_1','order'=>7),
                         array("user_field_id"=>15,'role_id'=>8,'step'=>'step_1','order'=>8),
 
-                        array("user_field_id"=>24,'role_id'=>8,'step'=>'step_2','order'=>9),
-                        array("user_field_id"=>25,'role_id'=>8,'step'=>'step_2','order'=>10),
+                        //array("user_field_id"=>24,'role_id'=>8,'step'=>'step_2','order'=>9),
+                        array("user_field_id"=>25,'role_id'=>8,'step'=>'step_1','order'=>9),
                         //Voyagers
                         array("user_field_id"=>16,'role_id'=>10,'step'=>'step_1','order'=>1),
                         array("user_field_id"=>17,'role_id'=>10,'step'=>'step_1','order'=>2),
@@ -154,8 +154,8 @@ class SeedUserFieldMapRolesTableSeeder extends Seeder
                         array("user_field_id"=>26,'role_id'=>10,'step'=>'step_2','order'=>8),
                         //array("user_field_id"=>8,'role_id'=>10,'step'=>'step_2','order'=>9),
                         array("user_field_id"=>15,'role_id'=>10,'step'=>'step_2','order'=>9),
-                        array("user_field_id"=>27,'role_id'=>10,'step'=>'step_2','order'=>10),
-                        array("user_field_id"=>25,'role_id'=>10,'step'=>'step_2','order'=>11),
+                        //array("user_field_id"=>27,'role_id'=>10,'step'=>'step_2','order'=>10),
+                        array("user_field_id"=>25,'role_id'=>10,'step'=>'step_2','order'=>10),
                         
                         //Voice Of Expert
                         array("user_field_id"=>16,'role_id'=>7,'step'=>'step_1','order'=>1),
