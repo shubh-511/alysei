79412211cd4dd5dabb8b5b549885a26634ab8a13
Shubham <shubham.kumar@ibyteinfomatics.com>
2021-03-09 16:36:04 +0530

    update user settings

--- Modules/User/Entities/User.php
+++ Modules/User/Entities/User.php
@@ -7,6 +7,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Passport\HasApiTokens;
 use Modules\User\Entities\Role;
+use App\Attachment;
 
 class User extends Authenticatable
 {
