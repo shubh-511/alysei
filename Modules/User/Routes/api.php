a6e161fd80ff28e34b35497d29fc8785b482287a
Shubham <shubham.kumar@ibyteinfomatics.com>
2021-04-02 12:33:23 +0530

    added about in importer and dis

--- Modules/User/Routes/api.php
+++ Modules/User/Routes/api.php
@@ -57,7 +57,7 @@ Route::group(['middleware' => 'auth:api'], function(){
 	Route::post('update/user/certificates', 'Api\UserController@updateUserCertificates');
 
 	Route::get('get/member/profile', 'Api\UserController@getMemberProfile');
-	Route::get('get/tab/about', 'Api\UserController@getTabAbout');
+	Route::get('get/member/about/tab', 'Api\UserController@getMemberAboutTab');
 	Route::get('get/tab/contact', 'Api\UserController@getTabContact');
 
 
