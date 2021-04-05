526cfbef564c9c1d82a359b89b7417b48d2c1e17
Shubham <shubham.kumar@ibyteinfomatics.com>
2021-04-05 17:25:10 +0530

    added profile status trait

---
+++ app/Http/Traits/ProfileStatusTrait.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace App\Http\Traits;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User; 
+use Carbon\Carbon;
+use Modules\User\Entities\User;
+use Modules\User\Entities\FeaturedListing;
+use Modules\User\Entities\UserSelectedHub;
+use Modules\User\Entities\UserTempHub;
+use App\Attachment;
+use Illuminate\Support\Facades\Auth; 
+use Validator;
+//use App\Events\UserRegisterEvent;
+
+trait ProfileStatusTrait
+{
+    
+    /***
+    Get Profile Status
+    ***/
+    public function profileStatus($userId)
+    {
+        $profilePercentage = '';
+
+        $user = User::where('user_id', $userId)->first();  // 10%
+        $FeaturedListing = FeaturedListing::where('user_id', $userId)->count();  // 25%
+        $userSelectedHub = UserSelectedHub::where('user_id', $userId)->count();  // 25%
+        $userTempHub = UserTempHub::where('user_id', $userId)->count();
+
+        if(empty($user->cover_id) && empty($user->avatar_id) && empty($user->about) && count($FeaturedListing) == 0 && (count($userSelectedHub) == 0 || count($userTempHub) == 0))
+        {
+            $profilePercentage = "25";
+        }
+        elseif(!empty($user->cover_id) && empty($user->avatar_id) && empty($user->about) && count($FeaturedListing) == 0 && (count($userSelectedHub) == 0 || count($userTempHub) == 0))
+        {
+            $profilePercentage = "30";
+        }
+        elseif(!empty($user->avatar_id) && empty($user->cover_id) && empty($user->about) && count($FeaturedListing) == 0 && (count($userSelectedHub) == 0 || count($userTempHub) == 0))
+        {
+            $profilePercentage = "30";
+        }
+        elseif(empty($user->avatar_id) && empty($user->cover_id) && !empty($user->about) && count($FeaturedListing) == 0 && (count($userSelectedHub) == 0 || count($userTempHub) == 0))
+        {
+            $profilePercentage = "40";
+        }
+        elseif(empty($user->avatar_id) && empty($user->cover_id) && empty($user->about) && count($FeaturedListing) > 0 && (count($userSelectedHub) == 0 || count($userTempHub) == 0))
+        {
+            $profilePercentage = "50";
+        }
+        elseif(empty($user->avatar_id) && empty($user->cover_id) && empty($user->about) && count($FeaturedListing) == 0 && (count($userSelectedHub) > 0 || count($userTempHub) > 0))
+        {
+            $profilePercentage = "50";
+        }
+
+
+
+
+        /***/
+        
+        elseif(!empty($user->avatar_id) && !empty($user->cover_id) && empty($user->about) && count($FeaturedListing) == 0 && (count($userSelectedHub) == 0 || count($userTempHub) == 0))
+        {
+            $profilePercentage = "35";
+        }
+        elseif(!empty($user->avatar_id) && empty($user->cover_id) && !empty($user->about) && count($FeaturedListing) == 0 && (count($userSelectedHub) == 0 || count($userTempHub) == 0))
+        {
+            $profilePercentage = "45";
+        }
+        elseif(!empty($user->avatar_id) && empty($user->cover_id) && empty($user->about) && count($FeaturedListing) > 0 && (count($userSelectedHub) == 0 || count($userTempHub) == 0))
+        {
+            $profilePercentage = "55";
+        }
+        elseif(!empty($user->avatar_id) && empty($user->cover_id) && empty($user->about) && count($FeaturedListing) == 0 && (count($userSelectedHub) > 0 || count($userTempHub) > 0))
+        {
+            $profilePercentage = "55";
+        }
+        /***/
+
+        elseif(empty($user->avatar_id) && !empty($user->cover_id) && !empty($user->about) && count($FeaturedListing) == 0 && (count($userSelectedHub) == 0 || count($userTempHub) == 0))
+        {
+            $profilePercentage = "45";
+        }
+       
+        elseif(empty($user->avatar_id) && !empty($user->cover_id) && empty($user->about) && count($FeaturedListing) > 0 && (count($userSelectedHub) == 0 || count($userTempHub) == 0))
+        {
+            $profilePercentage = "55";
+        }
+        elseif(empty($user->avatar_id) && !empty($user->cover_id) && empty($user->about) && count($FeaturedListing) == 0 && (count($userSelectedHub) > 0 || count($userTempHub) > 0))
+        {
+            $profilePercentage = "55";
+        }
+        /***/
+        elseif(empty($user->avatar_id) && !empty($user->cover_id) && !empty($user->about) && count($FeaturedListing) == 0 && (count($userSelectedHub) == 0 || count($userTempHub) == 0))
+        {
+            $profilePercentage = "45";
+        }
+        elseif(empty($user->avatar_id) && empty($user->cover_id) && !empty($user->about) && count($FeaturedListing) > 0 && (count($userSelectedHub) == 0 || count($userTempHub) == 0))
+        {
+            $profilePercentage = "55";
+        }
+        elseif(empty($user->avatar_id) && empty($user->cover_id) && !empty($user->about) && count($FeaturedListing) == 0 && (count($userSelectedHub) > 0 || count($userTempHub) > 0))
+        {
+            $profilePercentage = "65";
+        }
+        
+        /****/
+        elseif(!empty($user->avatar_id) && empty($user->cover_id) && empty($user->about) && count($FeaturedListing) > 0 && (count($userSelectedHub) == 0 || count($userTempHub) == 0))
+        {
+            $profilePercentage = "55";
+        }
+        elseif(empty($user->avatar_id) && !empty($user->cover_id) && empty($user->about) && count($FeaturedListing) > 0 && (count($userSelectedHub) == 0 || count($userTempHub) == 0))
+        {
+            $profilePercentage = "55";
+        }
+        elseif(empty($user->avatar_id) && empty($user->cover_id) && !empty($user->about) && count($FeaturedListing) > 0 && (count($userSelectedHub) == 0 || count($userTempHub) == 0))
+        {
+            $profilePercentage = "65";
+        }
+        elseif(empty($user->avatar_id) && empty($user->cover_id) && empty($user->about) && count($FeaturedListing) > 0 && (count($userSelectedHub) > 0 || count($userTempHub) > 0))
+        {
+            $profilePercentage = "75";
+        }
+        /***/
+
+        elseif(!empty($user->avatar_id) && !empty($user->cover_id) && !empty($user->about) && count($FeaturedListing) == 0 && (count($userSelectedHub) == 0 || count($userTempHub) == 0))
+        {
+            $profilePercentage = "50";
+        }
+        elseif(!empty($user->avatar_id) && !empty($user->cover_id) && !empty($user->about) && count($FeaturedListing) == 0 && (count($userSelectedHub) == 0 || count($userTempHub) == 0))
+        {
+            $profilePercentage = "50";
+        }
+
+        elseif(!empty($user->avatar_id) && !empty($user->cover_id) && empty($user->about) && count($FeaturedListing) > 0 && (count($userSelectedHub) == 0 || count($userTempHub) == 0))
+        {
+            $profilePercentage = "60";
+        }
+        elseif(!empty($user->avatar_id) && !empty($user->cover_id) && empty($user->about) && count($FeaturedListing) == 0 && (count($userSelectedHub) > 0 || count($userTempHub) > 0))
+        {
+            $profilePercentage = "60";
+        }
+
+        elseif(empty($user->avatar_id) && !empty($user->cover_id) && !empty($user->about) && count($FeaturedListing) > 0 && (count($userSelectedHub) == 0 || count($userTempHub) == 0))
+        {
+            $profilePercentage = "70";
+        }
+
+        elseif(empty($user->avatar_id) && !empty($user->cover_id) && !empty($user->about) && count($FeaturedListing) == 0 && (count($userSelectedHub) > 0 || count($userTempHub) > 0))
+        {
+            $profilePercentage = "70";
+        }
+
+        elseif(empty($user->avatar_id) && empty($user->cover_id) && !empty($user->about) && count($FeaturedListing) > 0 && (count($userSelectedHub) > 0 || count($userTempHub) > 0))
+        {
+            $profilePercentage = "90";
+        }
+
+        elseif(!empty($user->avatar_id) && empty($user->cover_id) && empty($user->about) && count($FeaturedListing) > 0 && (count($userSelectedHub) > 0 || count($userTempHub) > 0))
+        {
+            $profilePercentage = "90";
+        }
+
+
+        
+        else
+        {
+            $profilePercentage = "100";
+           
+        }
+        $userUpdate = User::where('user_id', $userId)->update(['profile_percentage' => $profilePercentage]);
+
+        return $profilePercentage;
+    }
+    
+
+}
