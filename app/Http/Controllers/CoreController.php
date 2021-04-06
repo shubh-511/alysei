fbf40ce8fb6d38431ee953d777a5d2853182b046
Shubham <shubham.kumar@ibyteinfomatics.com>
2021-04-06 11:29:22 +0530

    added userfields array data

--- app/Http/Controllers/CoreController.php
+++ app/Http/Controllers/CoreController.php
@@ -5,7 +5,7 @@ use Lang;
 
 class CoreController extends Controller
 {
-    public $userFieldsArray = ['user_id', 'name', 'email','first_name','last_name','middle_name','phone','postal_code','last_login_date','roles'];
+    public $userFieldsArray = ['user_id', 'name', 'email','first_name','last_name','middle_name','phone','postal_code','last_login_date','timezone','locale','vat_no','company_name','restaurant_name','roles'];
 
     
     public function translate($id, $fallback = null)
