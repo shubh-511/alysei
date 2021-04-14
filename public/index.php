0b308881fdfaada7a91984cf22a71f54e4c2aa30
shubham <shubham@gmail.com>
2021-04-14 06:53:51 +0000

    added cors

--- public/index.php
+++ public/index.php
@@ -6,6 +6,9 @@
  * @package  Laravel
  * @author   Taylor Otwell <taylor@laravel.com>
  */
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: *');
 
 define('LARAVEL_START', microtime(true));
 
